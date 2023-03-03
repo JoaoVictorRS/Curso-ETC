70cfa82a054dfed814329ae9b347c3fdb4359957
João Victor <joaososteam@gmail.com>
2023-03-02 21:30:25 -0300

    Session added

--- LV2/resultado.php
+++ LV2/resultado.php
@@ -1,4 +1,5 @@
 <?php
+    session_start();
 
     function verificarDados(){
         if(isset($_POST['email']) && empty($_POST['email'])){
@@ -11,15 +12,23 @@
     }
 
     function verificarUsuario(){
-        if(verificarDados() == true){
-            strtolower(trim($_POST['email']));
-            strtolower(trim($_POST['password']));
-            return true;
-        };
+            if(verificarDados() == true){
+                $_SESSION['email'] = strtolower(trim($_POST['email']));
+                $_SESSION['password'] = strtolower(trim($_POST['password']));
+                //EU TINHA LIDO ERRADO AQUI AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA
+                $email_db = 'usuario@gmail.com';
+                $password_db = '123';
+
+                    if($email_db == $_SESSION['email'] && $password_db == $_SESSION['password']){
+                    return true;
+            };
+        }
     }
 
     if(verificarDados() == true && verificarUsuario() == true){
         echo "Bem vindo!";
+        print_r($_SESSION);
+
     }
 
     echo "<br><a href='index.php'>Voltar para pagina anterior</a>";
